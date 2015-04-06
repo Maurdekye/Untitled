d8e75dce3475208f882cf07ef4e8bd9772d8c219
exploderguy <Dieckmann@Cathy-Dieckmanns-iMac.local>
2015-04-06 14:50:10 +1000

    Macs are dumb

--- gamemode/modules/ExampleModule/examplefile.lua
+++ gamemode/modules/ExampleModule/examplefile.lua
@@ -1,6 +1,6 @@
-—Dicks are good when they are small.
+// Dicks are good when they are small.
 
-if CLIENT then — Im not sure wether i will include the files shared.
+if CLIENT then // Im not sure wether i will include the files shared.
    timer.Simple(1, function()
 	LocalPlayer():ChatPrint( ‘Dikkies’ )
    end)
