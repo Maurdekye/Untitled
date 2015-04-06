53782cf1cb05c526ac76779300f225ee1bc28e60
Maurdekye <arbitrossal@gmail.com>
2015-04-06 02:44:30 -0400

    Added victim teleport script

---
+++ gamemode/modules/Maurdekye/README.md
@@ -0,0 +1,4 @@
+# Teleport to Victim
+
+This is a painfully simple script that teleports you to the exact position and angle of the player
+you kill.
---
+++ gamemode/modules/Maurdekye/teleport_to_victim.lua
@@ -0,0 +1,7 @@
+if SERVER then
+	hook.Add("PlayerDeath", "teleport_to_victim_death", function(killed, method, attacker)
+		if attacker:IsPlayer() and attacker ~= killed and attacker:Alive() then
+			attacker:SetPos( killed:GetPos() )
+		end
+	end )
+end
