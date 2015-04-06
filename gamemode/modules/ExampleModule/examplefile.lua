cffce1244fc5d7bff81acbacbba79b0eb0e48718
exploderguy <Dieckmann@Cathy-Dieckmanns-iMac.local>
2015-04-06 14:39:10 +1000

    Example
    Example

---
+++ gamemode/modules/ExampleModule/examplefile.lua
@@ -0,0 +1,8 @@
+—Dicks are good when they are small.
+
+if CLIENT then — Im not sure wether i will include the files shared.
+   timer.Simple(1, function()
+	LocalPlayer():ChatPrint(“Dikkies”)
+   end)
+end
+
