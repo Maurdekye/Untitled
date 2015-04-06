f97a187bfebbc08a67b33ba466f5be2cd191294d
AnonTakesOver <AnonTakesOver@live.com>
2015-04-07 08:13:48 +1000

    Update cl_360.lua
    Hard to edit on my bloody phone!!!

--- gamemode/modules/r0uge/cl_360.lua
+++ gamemode/modules/r0uge/cl_360.lua
@@ -1,29 +1,31 @@
 AddCSLuaFile()
 
+local __spin360_time, __spin360, __yaw, __newyaw, CLHasDone360
+
 
 function Spin360(time)
-	self.__spin360_time = time
-	self.__spin360 = true
-	self.__yaw = self:GetAngles( ).y
-  self.__newyaw = self.__yaw + 360
-	self.CLHasDoneA360 = true
+	__spin360_time = time
+	__spin360 = true
+	__yaw = self:GetAngles( ).y
+  __newyaw = __yaw + 360
+	CLHasDoneA360 = true
 	timer.Simple(0.5, function()
-		self.CLHasDoneA360 = false
+		CLHasDoneA360 = false
 	end)
 end
 
 local view
 local function CalcView( pl, pos, ang, fov )
-	if pl.__spin360 then
+	if __spin360 then
 	view = GAMEMODE:CalcView( pl, pos, ang, fov )
 	
-	pl.__yaw = math.Clamp(pl.__yaw + FrameTime( ) * pl.__spin360_time * 360,pl.__yaw,pl.__newyaw)
+	__yaw = math.Clamp(__yaw + FrameTime( ) * __spin360_time * 360,__yaw, __newyaw)
 	
-	if pl.__yaw == pl.__newyaw then
-		pl.__spin360 = false
+	if __yaw == __newyaw then
+		__spin360 = false
 	end
 	
-	view.angles.y = pl.__yaw
+	view.angles.y = __yaw
  
 	return view
 	end
