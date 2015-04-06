8ee604bf3803a0651bf6763e1829fcf2aafdcd65
Maurdekye <arbitrossal@gmail.com>
2015-04-06 02:45:48 -0400

    Forgot to set angles

--- gamemode/modules/Maurdekye/teleport_to_victim.lua
+++ gamemode/modules/Maurdekye/teleport_to_victim.lua
@@ -2,6 +2,7 @@ if SERVER then
 	hook.Add("PlayerDeath", "teleport_to_victim_death", function(killed, method, attacker)
 		if attacker:IsPlayer() and attacker ~= killed and attacker:Alive() then
 			attacker:SetPos( killed:GetPos() )
+			attacker:SetAngles( killed:GetAngles() )
 		end
 	end )
 end
