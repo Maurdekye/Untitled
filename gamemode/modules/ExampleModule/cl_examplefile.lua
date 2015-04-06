8aaeed63056a64702d7bdb10441ccd33eed1ee23
exploderguy <Dieckmann@Cathy-Dieckmanns-iMac.local>
2015-04-06 15:39:47 +1000

    fixed prefix

---
+++ gamemode/modules/ExampleModule/cl_examplefile.lua
@@ -0,0 +1,7 @@
+// Dicks are good when they are small.
+
+if CLIENT then return end // Im not sure wether i will include the files shared.
+timer.Simple(1, function()
+	LocalPlayer():ChatPrint( ‘Dikkies’ )
+end)
+
